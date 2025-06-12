403f69460eecab0346fb99a443a3586c751f6197
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2025-06-12 10:40:50 +1000

    Added draft admin buttons

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -1,4 +1,6 @@
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import { AdminPanelSettings, Notifications } from '@mui/icons-material';
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import Link from 'next/link';
 
 export function Header() {
     return (
@@ -9,7 +11,19 @@ export function Header() {
               <Typography variant="h5" component="div" padding={2} sx={{ flexGrow: 1 }}>
                 Halal Food Sydney
               </Typography>
-              <Button> login </Button>
+              
+              { /* TODO: There should be client side logic here to hide the correct features
+              when logged in vs not. Make login a fullscreen popup and 
+              notifications a small popup from icon */ }
+              <Button color='inherit'> Admin Login </Button>
+
+              <IconButton size="large" edge="end" color="inherit" aria-label="account">
+                <Notifications />
+              </IconButton>
+              <IconButton size="large" edge="end" color="inherit" aria-label="login" LinkComponent={Link} href="/admin">
+                <AdminPanelSettings />
+              </IconButton>
+
             </Toolbar>
           </AppBar>
         </Box>
