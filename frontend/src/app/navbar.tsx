e3b463bd006442b25aeb5f21fe7d2026024ae44e
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2025-06-12 10:40:28 +1000

    Fixed broken link

--- frontend/src/app/navbar.tsx
+++ frontend/src/app/navbar.tsx
@@ -5,7 +5,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import Link from "next/link";
 
 export function NavBar() {
-    
+
     // TODO: These use link and are thus prefetching - think about whether this is needed in terms of 
     //  firebase document reads (Are we going to read once after location services are enabled or
     //  location input first and thus we page our results and keep these in memory so we can prefetch
@@ -15,7 +15,7 @@ export function NavBar() {
         <>
         <Box sx={{ position: "fixed", bottom: 0, left: 0, right: 0}}>
         <BottomNavigation showLabels > 
-          <BottomNavigationAction label="Map" icon={<MapIcon />} LinkComponent={Link} href="/map"/>
+          <BottomNavigationAction label="Map" icon={<MapIcon />} LinkComponent={Link} href="/"/>
           <BottomNavigationAction label="List" icon={<ListAllIcon />} LinkComponent={Link} href="/list" />
           <BottomNavigationAction label="Settings" icon={<SettingsIcon />} LinkComponent={Link} href="/settings" />
         </BottomNavigation>
