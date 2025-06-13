1b9c63e72d7e098d1e9a8c407b2ef5730856efd0
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2025-06-13 10:34:40 +1000

    Setup background for User pages

---
+++ frontend/src/app/listing/[entry]/page.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+export function ListEntryPage() {
+    return (
+        <Box sx={{ position: "fixed", top: 64, left: 0, width: "100vw", height: "calc(100vh - 64px - 56px)", 
+        overflow: "hidden", backgroundColor: "lightGray", display: "flex", 
+        flexDirection:"column", justifyContent: "space-evenly", alignItems: "center"}}>
+            
+        </Box>
+    );
+}
+
+export default ListEntryPage;
