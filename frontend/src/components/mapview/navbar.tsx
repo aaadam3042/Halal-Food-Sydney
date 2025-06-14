b41252dcf7c0dbf3e26bc0f1f9456dc5ee5fef17
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2025-06-14 14:44:18 +1000

    Added comment for bug

--- frontend/src/components/mapview/navbar.tsx
+++ frontend/src/components/mapview/navbar.tsx
@@ -10,6 +10,8 @@ export function NavBar() {
 
     const [currentNav, setCurrentNav] = React.useState(0);
 
+    // TODO: This only sets the currentNav to home page only. Need to fix
+
     // TODO: These use link and are thus prefetching - think about whether this is needed in terms of 
     //  firebase document reads (Are we going to read once after location services are enabled or
     //  location input first and thus we page our results and keep these in memory so we can prefetch
