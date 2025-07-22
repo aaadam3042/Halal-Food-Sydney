fda1c97a70d6824f1ae2137d8c733fc1ee492188
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2025-07-22 17:47:29 +1000

    Fixed map page crash from filters needing state and removed listing entries page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
+'use client'; // TODO: Do we want this to be a client component?
+
+import React from "react";
+import { LocationFiltersTypes } from "@/components/locationfilters";
 import MapView from "../components/mapview/mapview";
 import LocationBar from "../components/mapview/locationbar";
 import { Box } from "@mui/material";
@@ -5,6 +9,7 @@ import LocationFilters from "../components/locationfilters";
 import SummaryCard from "@/components/mapview/summarycard";
 
 export default function Home() {
+  const [filters, setFilters] = React.useState<LocationFiltersTypes[]>([LocationFiltersTypes.Restaurant, LocationFiltersTypes.Butcher]);
 
   return (
     <>
@@ -14,7 +19,7 @@ export default function Home() {
         alignItems: "center", backgroundImage: "url(/geometric-background.png)",
         backgroundSize: "30%", backgroundPosition: "center", backgroundRepeat: "repeat"}}>
 
-        <LocationFilters />
+        <LocationFilters filters={filters} setFilters={setFilters} />
         <MapView />
         <SummaryCard />
 
