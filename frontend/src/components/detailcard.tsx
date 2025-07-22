6fd0d91c975147858b0394a0e790aee370aff5b2
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2025-07-22 18:21:46 +1000

    Added contacts section to detail card

--- frontend/src/components/detailcard.tsx
+++ frontend/src/components/detailcard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Card, ClickAwayListener, Divider, IconButton, Typography } from "@mui/material";
+import { Box, Card, ClickAwayListener, Divider, IconButton, Link, Typography } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import { FoodService } from "@/types/foodService";
 import { StatusChip, StatusType } from "./statuschip";
@@ -35,7 +35,9 @@ export function DetailCard({foodService, active, onClose}: DetailCardProps) {
             width: "50vw",
             borderRadius: "10px",
             zIndex: 2,
-            backgroundColor: cardColour[foodService.type]
+            backgroundColor: cardColour[foodService.type],
+            overflowY: "auto",
+            overflowX: "hidden",
         }}>
 
             {/* TODO: Add rest of data */}
@@ -51,12 +53,41 @@ export function DetailCard({foodService, active, onClose}: DetailCardProps) {
                 </Box>
 
                 <Typography> {foodService?.address} </Typography>
-                <Divider sx={{ marginY: 1 }} />
+                <Divider sx={{ marginY: 2 }} />
 
                 <Box sx={{ display: 'flex', flexDirection: "row", gap: 1, mt: 1 }}>
-                <StatusChip status={foodService?.status.generalHalal} type={StatusType.General} />
-                <StatusChip status={foodService?.status.handslaughtered} type={StatusType.Handslaughtered} />
+                    <StatusChip status={foodService?.status.generalHalal} type={StatusType.General} />
+                    <StatusChip status={foodService?.status.handslaughtered} type={StatusType.Handslaughtered} />
                 </Box>
+
+                <Divider sx={{ mt: 3 }} />
+
+                { foodService.notes.trim() !== "" ? 
+                    <>
+                    <Typography variant="subtitle1" sx={{ fontWeight: "bold", marginTop: 1 }}> Notes: </Typography>
+                    <Typography variant="body1" sx={{ marginTop: 1, whiteSpace: "pre-line" }}>
+                        {foodService.notes}
+                    </Typography>
+                    </> 
+                    : 
+                    <></>
+                }
+
+                Last contacted on - Should we modify our work with last updated as well (would need to change document schema)
+
+                <Divider sx={{ my: 3 }} /> {/* Note that this will put double divider without contact info and notes. (but always will be last contact wiht n/a??) */}
+                
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, mt: 1 }}>
+                    {foodService.contact?.phone && <Typography> Phone: {foodService.contact.phone} </Typography>}
+                    {foodService.contact?.email && <Typography> Email: <Link 
+                        href={`mailto:${foodService.contact.email}?subject=Halal\ Enquiry`}> {foodService.contact.email} </Link> </Typography>}
+                    {foodService.contact?.website && <Typography> Website: <Link target="_blank" rel="noreferrer" 
+                        href={`https://${foodService.contact.website}`}> {foodService.contact.website} </Link> </Typography>}
+                </Box>
+
+                Button for history
+
+                Button open in map (Just do google maps with lat and long? Harder to get internal map specific without native)
 
             </Box>
 
